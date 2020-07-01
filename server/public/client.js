e5765be2d6706e7807278f69ded01266f8f80239
Kevin Anderson <kmichael419@hotmail.com>
2020-07-01 14:07:32 -0500

    arrays showing on main localhost route but not the proper /artist and /songs

--- server/public/client.js
+++ server/public/client.js
@@ -1,21 +1,40 @@
 $(document).ready(onReady);
-
+// when the document is ready, an ajax is being sent to the server and a 'GET' request is being performed asking for the artist array.
 function onReady() {
-    $.ajax({
-        type: 'GET',
-        url: '/artist'
-    }).then(function (response) {
-        for (let i = 0; i < response.length; i++) {
-            let artist = response[i];
-            $('#artistTableBody').append(`
+  $.ajax({
+    type: 'GET',
+    url: '/artist',
+    //once the request is received by the server,
+    // then the server sends back the artist array and each object of the
+    //artist array (each artist) is appended to the DOM into a table that includes each object's info.
+  }).then(function (response) {
+    for (let i = 0; i < response.length; i++) {
+      let artist = response[i];
+      console.log(artist);
+
+      $('#artistTableBody').append(`
                 <tr>
                     <td>${artist.name}</td>
                     <td>${artist.born}</td>
                     <td>${artist.died}</td>
                 </tr>
             `);
-        }
-    });
+    }
+  });
 
-    // TODO Add ajax request for /songs and display on DOM
+  // TODO Add ajax request for /songs and display on DOM
+  $.ajax({
+    type: 'GET',
+    url: '/songs',
+  }).then(function (response) {
+    for (let i = 0; i < response.length; i++) {
+      let songs = response[i];
+      $('#songTableBody').append(`
+                  <tr>
+                      <td>${songs.title}</td>
+                      <td>${songs.artist}</td>
+                  </tr>
+              `);
+    }
+  });
 }
